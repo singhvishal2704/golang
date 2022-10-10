7e00fd8632a1277c6641271cf937c9218f823575
Vishal Singh <singhvishal2704@gmail.com>
2022-10-10 11:58:26 +0530

    Add files via upload

---
+++ functions/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Welcome to functions in golang")
+	greeter()
+	greeterTwo()
+
+	result := adder(3, 5)
+
+	fmt.Println("Result is: ", result)
+
+	proRes, myMessage := proAdder(2, 5, 8, 7, 3)
+	fmt.Println("Pro result is: ", proRes)
+	fmt.Println("Pro Message is: ", myMessage)
+}
+
+func adder(valOne int, valTwo int) int {
+	return valOne + valTwo
+}
+
+func proAdder(values ...int) (int, string) {
+	total := 0
+	for _, val := range values {
+		total += val
+	}
+	return total, "Hi Pro result function"
+}
+
+func greeterTwo() {
+	fmt.Println("Another method")
+}
+
+func greeter()  {
+	fmt.Println("Namaste from golang")
+}
